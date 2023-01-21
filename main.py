3536421cf5add6febc6a6998ceaf5e0f818aeb70
Levko <lbeniakh@kse.org.ua>
2023-01-21 20:31:24 +0200

    Higher rate

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ not_na = titles["imdb_score"].notna()
 new_table = titles[not_na]
 rated_movies = titles.loc[not_na & (titles["type"] == "MOVIE"), "imdb_score"]
 rated_shows = titles.loc[not_na & (titles["type"] == "SHOW"), "imdb_score"]
-print(rated_movies, rated_shows, sep="\n")
+print(rated_movies, rated_shows, rated_movies.mean(), rated_shows.mean(), sep="\n")
 
 plt.subplot(1, 2, 1)
 plt.hist(rated_movies, bins=np.arange(min(rated_movies), max(rated_movies) + 0.2, 0.2))
