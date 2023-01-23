d177d1c61daa0bd79c068c5f20f0a1383b0e9d1d
Levko <lbeniakh@kse.org.ua>
2023-01-23 17:15:51 +0200

    Improvements

--- main.py
+++ main.py
@@ -1,19 +1,35 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+import seaborn as sns
 
 credits_frame = pd.read_csv("credits.csv")
 titles = pd.read_csv("titles.csv")
 
-not_na = titles["imdb_score"].notna()
-new_table = titles[not_na]
-rated_movies = titles.loc[not_na & (titles["type"] == "MOVIE"), "imdb_score"]
-rated_shows = titles.loc[not_na & (titles["type"] == "SHOW"), "imdb_score"]
-print(rated_movies, rated_shows, rated_movies.mean(), rated_shows.mean(), sep="\n")
+# first part
+
+rated_movies = titles[titles["type"] == "MOVIE"]["imdb_score"].dropna()
+rated_shows = titles[titles["type"] == "SHOW"]["imdb_score"].dropna()
 
 plt.subplot(1, 2, 1)
-plt.hist(rated_movies, bins=np.arange(min(rated_movies), max(rated_movies) + 0.2, 0.2))
+sns.histplot(titles, x="imdb_score", bins=np.arange(0, 10.2, 0.2), hue="type")
+plt.xlabel("Score")
+plt.ylabel("Amount")
+plt.axvline(rated_movies.mean(), color="k", linestyle='dashed', linewidth=1)
+plt.axvline(rated_shows.mean(), color="#123456", linestyle='dashdot', linewidth=1)
+
+# second part
 
 plt.subplot(1, 2, 2)
-plt.hist(rated_shows, bins=np.arange(min(rated_shows), max(rated_shows) + 0.2, 0.2))
+age_certifications = titles[titles['type'] == 'SHOW']['age_certification'].dropna()
+labels, values = np.unique(age_certifications, return_counts=True)
+plt.pie(values, labels=labels)
+
+# fourth part
+
+double_table = pd.merge(titles.sort_values(by="imdb_score", ascending=False).head(1000),
+                        credits_frame[credits_frame['role'] == "ACTOR"], how="inner", on=["id"])
+actor_rating = double_table.groupby(by="name")['title'].count().sort_values(ascending=False).head(10)
+
+print(titles.groupby(by=["type"])["imdb_score"].mean(), double_table, actor_rating, sep="\n")
 plt.show()
