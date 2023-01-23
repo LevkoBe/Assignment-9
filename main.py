2fd9ab9fd00ff3c348ea2db339fe0a0e612f9d02
Denis Pasichnyk <119937568+DenisPasichnyk@users.noreply.github.com>
2023-01-23 21:48:27 +0200

    add size

--- main.py
+++ main.py
@@ -10,17 +10,18 @@ new_table = titles[not_na]
 rated_movies = titles.loc[not_na & (titles["type"] == "MOVIE"), "imdb_score"]
 rated_shows = titles.loc[not_na & (titles["type"] == "SHOW"), "imdb_score"]
 print(rated_movies, rated_shows, rated_movies.mean(), rated_shows.mean(), sep="\n")
-
-plt.subplot(1, 3, 1)
+plt.figure(figsize=(10,5))
+plt.subplot(1, 3, 2)
 
 age_certification = titles[titles["type"] == "SHOW"]["age_certification"].dropna()
 labels, values = np.unique(age_certification, return_counts=True)
 
 plt.pie(values, labels=labels)
 
-plt.subplot(1, 3, 2)
+plt.subplot(1, 3, 1)
 plt.hist(rated_movies, bins=np.arange(min(rated_movies), max(rated_movies) + 0.2, 0.2))
 
 plt.subplot(1, 3, 3)
 plt.hist(rated_shows, bins=np.arange(min(rated_shows), max(rated_shows) + 0.2, 0.2))
 plt.show()
+print(age_certification)
