7f382e2b80ce2745f08a0b75170749e4f737e1a7
Denis Pasichnyk <119937568+DenisPasichnyk@users.noreply.github.com>
2023-01-23 21:26:02 +0200

    add pie-chart

--- main.py
+++ main.py
@@ -11,9 +11,16 @@ rated_movies = titles.loc[not_na & (titles["type"] == "MOVIE"), "imdb_score"]
 rated_shows = titles.loc[not_na & (titles["type"] == "SHOW"), "imdb_score"]
 print(rated_movies, rated_shows, rated_movies.mean(), rated_shows.mean(), sep="\n")
 
-plt.subplot(1, 2, 1)
+plt.subplot(1, 3, 1)
+
+age_certification = titles[titles["type"] == "SHOW"]["age_certification"].dropna()
+labels, values = np.unique(age_certification, return_counts=True)
+
+plt.pie(values, labels=labels)
+
+plt.subplot(1, 3, 2)
 plt.hist(rated_movies, bins=np.arange(min(rated_movies), max(rated_movies) + 0.2, 0.2))
 
-plt.subplot(1, 2, 2)
+plt.subplot(1, 3, 3)
 plt.hist(rated_shows, bins=np.arange(min(rated_shows), max(rated_shows) + 0.2, 0.2))
 plt.show()
